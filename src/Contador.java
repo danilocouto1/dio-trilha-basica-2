fc37c14656a1f8ea492640a8993cefb90680c117
Dan <dansantosalfa@gmail.com>
2023-07-17 23:11:26 -0300

    Add - Contador entrada de dados

---
+++ src/Contador.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Programa contador que recebe dois valores via terminal e a diferenca deles é a quantidade de vezes que for será feito, implentação de exception personalizada
+ * 
+ * Author: Danilo Couto
+ */
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+        //Implementando o scanner
+        Scanner sc = new Scanner(System.in);
+
+        //Entrada de dados
+        System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = sc.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = sc.nextInt();
+
+        
+    }
+}
